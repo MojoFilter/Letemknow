b7c80432984b55b57461e73a7b096749d81a0a10
Josh Weeks <weeks.josh@gmail.com>
2023-10-06 19:00:11 -0400

    Fix link encoding and encapsulate it for good measure

---
+++ Letemknow/Shared/LEKConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Letemknow.Shared;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class LEKConfiguration
+{
+    public static IServiceCollection AddLEKClient(this IServiceCollection services) =>
+        services.AddTransient<ILinkBusiness, LinkBusiness>();
+}
